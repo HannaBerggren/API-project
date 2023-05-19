957ee42ac4f958cf33c472fd58471c85a430dc81
HannaBerggren <hannajberggren@gmail.com>
2023-05-19 11:47:54 +0000

    change debug to true

--- API_project/settings.py
+++ API_project/settings.py
@@ -53,7 +53,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'api-project-advanced-frontend.herokuapp.com']
 
